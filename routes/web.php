161899bf16caf3a5cf5931d2349fca92ec18759f
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-12 12:13:57 +0700

    Menampilkan Data Secara Langsung Melalui Route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/hello', function () {
+    $dataArray = [
+        'message' => 'Hello, world'
+    ];
+
+    // return $dataArray;
+    return response()->json($dataArray);
+});
