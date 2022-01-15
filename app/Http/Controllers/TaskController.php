13ecef3ea1f416bf6dbe8afac0933811351cd736
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-15 14:09:23 +0700

    Menambah Data dengan Query Builder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
@@ -23,9 +24,12 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
-        $this->tasklist[$request->label] = $request->task;
+        DB::table('tasks')->insert([
+            'task' => $request->task,
+            'user' => $request->user,
+        ]);
 
-        return $this->tasklist;
+        return 'success';
     }
 
     public function show($param)
