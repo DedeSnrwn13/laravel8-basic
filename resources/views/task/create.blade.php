fd245d5c3d60c7b0e92bce45d251d7d64ff34092
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-20 17:09:29 +0700

    Menampilkan Pesan Error Di Masing-Masing Field

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -2,16 +2,6 @@
 
 @section('main')
     <div class="mt-5 mx-auto" style="width: 380px">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         <div class="card">
             <div class="card-body">
                 <form action="{{ url('/tasks') }}" method="POST">
@@ -20,10 +10,16 @@
                     <div class="mb-3">
                         <label for="" class="form-label">User</label>
                         <input name="user" type="text" class="form-control">
+                        @error('user')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Task</label>
                         <textarea name="task" class="form-control" id="" rows="3"></textarea>
+                        @error('task')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
