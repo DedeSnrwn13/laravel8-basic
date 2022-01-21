f8eb1661fd665e603e99e9e961e7b6d5310224e7
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-21 14:03:48 +0700

    Mendapatkan Data Status Login Dan Mengaktifkan Logout

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -5,8 +5,19 @@
                 <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
             </ul>
             <div class="text-end">
-                <a href="#" class="btn btn-outline-light me-2">Login</a>
-                <a href="{{ url('register') }}" class="btn btn-warning">Sign-up</a>
+                @guest
+                    <a href="{{ route('login') }}" class="btn btn-outline-light me-2">Login</a>
+                    <a href="{{ route('register') }}" class="btn btn-warning">Sign-up</a>
+                @else
+                    <a href="{{ route('logout') }}" class="btn btn-outline-light me-2"
+                        onclick="event.preventDefault(); document.getElementById('form-logout').submit(); ">{{ Auth::user()->name }}</a>
+
+                    <form action="{{ route('logout') }}" id="form-logout" method="post">
+                        @csrf
+
+
+                    </form>
+                @endguest
             </div>
         </div>
     </div>
