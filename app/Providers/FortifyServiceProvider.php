6828689b38b79f72b3a0f56fc42915bdcaa04f4a
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-21 14:26:35 +0700

    Membuat Halaman Send Reset Password Link

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -53,5 +53,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function () {
             return view('auth.login');
         });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
     }
 }
